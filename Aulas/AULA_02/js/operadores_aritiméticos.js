6e3c76870011fe232649c4e3573cb7d841c77695
Dayane Estevão <ddestevao@gmail.com>
2023-04-02 11:07:24 -0300

    subindo aula02

---
+++ Aulas/AULA_02/js/operadores_aritiméticos.js
@@ -0,0 +1,30 @@
+var numero1 = 3
+var numero2 = 2
+
+var salarioAtual = 1000
+var reajusteSalarial = 10
+var valorReajuste = salarioAtual * reajusteSalarial / 100
+var salarioFinal = salarioAtual + valorReajuste
+
+console.log("--- ADIÇÃO---")
+console.log(numero1 , " + ", numero2 , " = ", numero1 + numero2)
+
+console.log("---SUBTRAÇÃO---")
+console.log(numero1 , " - ", numero2 , " = ", numero1 - numero2)
+
+console.log("---MULTIPLICAÇÃO---")
+console.log(numero1 , " * ", numero2 , " = ", numero1 * numero2)
+
+console.log("---DIVISÃO---")
+console.log(numero1 , " / ", numero2 , " = ", numero1 / numero2)
+
+console.log("---POTENCIAÇÃO---")
+console.log(numero1 , " ** ", numero2 , " = ", numero1 ** numero2)
+
+console.log("---RESTANTE DA DIVISÃO---")
+console.log(numero1 , " % ", numero2, " = ", numero1 % numero2)
+
+console.log("---PORCENTAGEM---")
+console.log(salarioAtual + salarioAtual * reajusteSalarial / 100)
+
+
---
+++ Configurações.git.md
@@ -0,0 +1,16 @@
+# GIT - Configurações
+
+### Configurar usuario
+    - Comandos
+        git config --global user.name "nome sobrenome"
+        git config --global user.email "pessoa@gmail.com"
+
+### Baixar o Repositório do github
+    - Comandos
+        git clone link-repositorio
+
+### Adicionar e subir alterações do repositório
+    git add
+    git commit -m "mensagem"
+    git push
+
